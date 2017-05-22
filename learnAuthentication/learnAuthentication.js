ec2af998cf75ee6cf9b3e83b57b75ed6eac2d12d
Divyanshu Singh <dsinecos@gmail.com>
2017-05-22 18:47:35 +0530

    Implemented basic authentication using passport-local strategy with session store in PostgreSQL

--- learnAuthentication/learnAuthentication.js
+++ learnAuthentication/learnAuthentication.js
@@ -1,33 +1,113 @@
-var express = require('express');
-var passport = require('passport'); // What does this do?
-var Strategy = require('passport-local').Strategy; // How does adding Strategy fit into the earlier Passport module?
-var db = require('./db'); // Configure this for PostgreSQL
+var express = require("express");
+var session = require('express-session');
+var passport = require("passport");
+var Strategy = require("passport-local").Strategy;
+var intermediate = require('connect-pg-simple')(session);
+var PostgreSqlStore = new intermediate({
+    conString: "pg://postgres:postgres@localhost:5432/testsession",
+    pruneSessionInterval: false
+});
 
-passport.use(new Strategy(function (username, password, cb) {
+var app = express();
+app.listen(2346);
 
-    // Verfiy function that uses (username, password) provided by the user and returns (err, userRecord) to the cb to attach it to the req object
+var pg = require('pg');
+var connectionString = "pg://admin:guest@localhost:5432/authentication";
+var client = new pg.Client(connectionString);
 
-    db.users.findByUsername(username, function (err, user) {
-        if (err) { return cb(err); }
-        if (!user) { return cb(null, false); } // Under what conditions would a record be returned that does not have user defined?
-        if (user.password != password) { return cb(null, false); }
-        return cb(null, user);
-    });
-}));
+client.connect();
+//client.query("DROP TABLE IF EXISTS authentication");
+client.query("CREATE TABLE IF NOT EXISTS authentication(id SERIAL PRIMARY KEY, username varchar(64), password varchar(64))");
 
-// What does it mean to serialize and deserialize users?
+app.use(require('body-parser').urlencoded({ extended: true }));
+var sessionOptions = {
+    secret: "Ghanta secret",
+    resave: false,
+    saveUninitialized: true,
+    name: "cookieDaNaam",
+    //store: PostgreSqlStore
+};
 
-passport.serializeUser(function(user, cb) { // What does this function do? // 
+app.use(session(sessionOptions));
+
+
+passport.use(new Strategy(
+    function (username, password, cb) {
+        console.log("Inside local strategy");
+        console.log("Value of Variables username " + username);
+        console.log("Value of Variables username " + password);
+        findByUsername(username, function (err, user) {
+            if (err) { return cb(err); }
+            if (!user) { return cb(null, false); }
+            if (user.password != password) { return cb(null, false); }
+            console.log("-----------------------------------------");
+            console.log("Inside findByUserName");
+            console.log("Value of variable username " + username);
+            return cb(null, user);
+        });
+    }));
+
+
+function findByUsername(username, fn) {
+    client.query("SELECT * FROM authentication WHERE username=$1", [username], returnedUsername);
+    //console.log("Inside findbyusername");
+    function returnedUsername(err, result) {
+        fn(err, result.rows[0]);
+    }
+}
+
+function findByID(id, fn) {
+    client.query("SELECT * FROM authentication WHERE id=$1", [id], returnedID);
+    console.log("Inside database findbyid");
+
+    function returnedID(err, result) {
+        console.log("Inside function returnedID");
+        console.log("result.rows[0] " + JSON.stringify(result.rows[0], null, "  "));
+        fn(err, result.rows[0]);
+    }
+
+}
+
+passport.serializeUser(function (user, cb) {
+    console.log("-----------------------------------------");
+    console.log("Inside serializeUser");
+    console.log("Variables user.id " + user.id);
     cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb){ // What does this function do?
-    db.users.findById(id, function(err, user) {
+passport.deserializeUser(function (id, cb) {
+    console.log("\n");
+    console.log("Inside deserializeUser");
+    console.log("What is the ID " + id);
+    console.log("\n");
+    findByID(id, function (err, user) {
         if (err) { return cb(err); }
         cb(null, user);
     });
 });
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.post('/',
+    passport.authenticate('local', { failureRedirect: '/failed' }),
+    function (req, res) {
+        console.log("Inside successful login");
+        res.send("Successfully logged in Hoyeeeee ");
 
+        console.log("req.session.passport.user " + req.session.passport.user);
+        console.log("req.user " + JSON.stringify(req.user, null, " "));
+    });
 
+app.get('/failed', function(req, res) {
+    res.send("Fail ho gaya re babua");
+})
+
+app.get('/', function(req, res) {
+    res.write("This is to test cookie hacking");
+    res.write("This is the name of the user " + JSON.stringify(req.user, null, "  "));
+    res.end();
+
+    console.log("This is to test cookie hacking");
+    console.log("The username is " + JSON.stringify(req.user, null, "  "));
+})
