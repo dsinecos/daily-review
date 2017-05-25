b8b297a8bfe7177f8c1f75c65e8aa02aab791195
Divyanshu Singh <dsinecos@gmail.com>
2017-05-25 16:46:51 +0530

    Data model for dailyreview App with the necessary constraints

--- dataModel.js
+++ dataModel.js
@@ -14,17 +14,31 @@ Tables for dailyreview app
  dailyreview_score 
 */
 
+/*
 client.query("DROP TABLE IF EXISTS dailyreview_category CASCADE");
 client.query("DROP TABLE IF EXISTS dailyreview_users CASCADE");
 client.query("DROP TABLE IF EXISTS dailyreview_userdate CASCADE");
 client.query("DROP TABLE IF EXISTS dailyreview_score CASCADE");
+client.query("DROP TABLE IF EXISTS dailyreview_journal CASCADE");
+client.query("DROP TABLE IF EXISTS dailyreview_journalresponse CASCADE");
+*/
 
-client.query("CREATE TABLE IF NOT EXISTS dailyreview_category(category_id SERIAL PRIMARY KEY, category_name varchar(64), category_label varchar(64))");
-client.query("CREATE TABLE IF NOT EXISTS dailyreview_users(user_id SERIAL PRIMARY KEY, user_name varchar(64))");
-client.query("CREATE TABLE IF NOT EXISTS dailyreview_userdate(user_id int, FOREIGN KEY (user_id) REFERENCES dailyreview_users(user_id), dateentry date, userdate_id SERIAL PRIMARY KEY)");
+client.query("CREATE TABLE IF NOT EXISTS dailyreview_users(user_id SERIAL PRIMARY KEY, user_name varchar(64), password varchar(64))");
+client.query("CREATE TABLE IF NOT EXISTS dailyreview_category(user_id int NOT NULL, FOREIGN KEY (user_id) REFERENCES dailyreview_users(user_id), category_id SERIAL PRIMARY KEY, category_name varchar(64), category_label varchar(64), UNIQUE (user_id, category_name))");
+client.query("CREATE TABLE IF NOT EXISTS dailyreview_userdate(user_id int, FOREIGN KEY (user_id) REFERENCES dailyreview_users(user_id), dateentry date NOT NULL, userdate_id SERIAL PRIMARY KEY, UNIQUE (user_id, dateentry))");
 client.query("CREATE TABLE IF NOT EXISTS dailyreview_score(userdate_id int, FOREIGN KEY (userdate_id) REFERENCES dailyreview_userdate(userdate_id), category_id int, FOREIGN KEY (category_id) REFERENCES dailyreview_category(category_id), score int)");
+client.query("CREATE TABLE IF NOT EXISTS dailyreview_journal(user_id int, FOREIGN KEY (user_id) REFERENCES dailyreview_users(user_id), journalquestion_id SERIAL PRIMARY KEY, journal_question text)");
+client.query("CREATE TABLE IF NOT EXISTS dailyreview_journalresponse(userdate_id int, FOREIGN KEY (userdate_id) REFERENCES dailyreview_userdate(userdate_id), journalquestion_id int, FOREIGN KEY (journalquestion_id) REFERENCES dailyreview_journal(journalquestion_id), journalquestionresponse text)");
 
+//client.query("insert into dailyreview_category (user_id, category_name, category_label) values (1, 'Ranee', 'Sales')");
+//client.query("insert into dailyreview_category (user_id, category_name, category_label) values (1, 'Ranee', 'Sales')");
+//client.query("SELECT category_name, category_label FROM dailyreview_category WHERE user_id=5", function(err, result) { console.log(result); });
+client.query("SELECT EXISTS (SELECT true FROM dailyreview_category WHERE category_name='Swiming')", function(err, result) {
+    console.log("This is the result of the boolean operation " + JSON.stringify(result, null, "  "));
+    console.log("This is the result of the boolean operation " + result.rows[0]['exists']);
+})
 
+/*
 // Inserting dummy data into dailyreview_category table
 var insert = [];
 
@@ -63,3 +77,4 @@ client.query("insert into dailyreview_score (userdate_id, category_id, score) va
 // Learn how to use multiple joins
 
 // Example Query - SELECT dailyreview_users.user_name as User, dailyreview_category.category_name as Category, dailyreview_userdate.dateentry as DataEntry, dailyreview_score.score as Score FROM dailyreview_users JOIN dailyreview_userdate ON (dailyreview_users.user_id = dailyreview_userdate.user_id) JOIN dailyreview_score ON (dailyreview_userdate.userdate_id = dailyreview_score.userdate_id) JOIN dailyreview_category ON (dailyreview_score.category_id = dailyreview_category.category_id);
+*/
