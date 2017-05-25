3367dffad092570d9f30bb921929410937e24b54
Divyanshu Singh <dsinecos@gmail.com>
2017-05-25 11:38:52 +0530

    Added error handling for user input and database querying

--- dailyReview.js
+++ dailyReview.js
@@ -209,10 +209,23 @@ app.get('/getCategory', checkAuthentication, function (req, res) {
                     WHERE user_id=$1`;
 
     dailyReviewClient.query(sqlQuery, [req.user.user_id]).then(function (data) {
-        res.send(JSON.stringify(data, null, "  "));
+        /*
+        var dataBoolean = data || false;
+        console.log("The boolean value of data is " + dataBoolean);
+        console.log("The value of data is " + JSON.stringify(data, null, "  "));
+        console.log("The typeof data is array? " + (data.constructor === Array));
+        console.log("The length of the array is " + data.length);
+        */
+        if(data.length === 0) {
+            res.send("There are no categories to display at this point");
+        } else {
+            res.send(JSON.stringify(data, null, "  "));
+        }
+        
     }).catch(function (error) {
-        res.send(error);
+        res.status(500).send("Internal server error ");
         console.log("Error retrieving categories from the database");
+        console.log(error);
     });
 });
 
